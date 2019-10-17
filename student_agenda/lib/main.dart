c33f14fdf33c7b478d48ccdaac56e47e559dff1e
Josh <JoshKPrier@gmail.com>
2019-10-17 03:20:22 -0400

    Added starter flutter code and a login UI

---
+++ student_agenda/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final email = TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Email",
+        border:
+          OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32.0),
+              borderSide: BorderSide(color: Colors.white),
+          ),
+      ),
+    );
+
+    final password = TextField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Password",
+        border:
+        OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {}, //TODO: Add login function
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    // This method is rerun every time setState is called
+    return Scaffold(
+      backgroundColor: Color(0xff00AC52),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 200.0,
+                child: Image.asset(
+                  "images/tdsblogo.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              SizedBox(height: 45.0),
+              email,
+              SizedBox(height: 20.0),
+              password,
+              SizedBox(height: 60.0),
+              loginButton,
+              SizedBox(height: 25.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
