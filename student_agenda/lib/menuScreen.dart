c1961f309883066413b5c0dc259e1207b97658e9
Viktar Chyhir <vchigir12@gmail.com>
2019-10-26 14:48:34 -0400

    Added drop down menu

--- student_agenda/lib/menuScreen.dart
+++ student_agenda/lib/menuScreen.dart
@@ -1,15 +1,122 @@
 import 'package:flutter/material.dart';
 import 'util.dart';
 
-class MainMenu extends StatelessWidget{
+//------------------------------
+//CODE ADAPTED FROM:  https://www.linkedin.com/pulse/build-your-own-custom-side-menu-flutter-app-alon-rom
+class MainMenu extends StatefulWidget {
+  @override
+  MainMenuState createState() => MainMenuState();
+
+}
+class MainMenuState extends State<MainMenu> {
+  Widget appBarTitle;
+  MenuItem selectedMenuItem;
+  List<MenuItem> menuItems;
+  List<Widget> menuOptionWidgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    menuItems = createMenuItems();
+    selectedMenuItem = menuItems.first;
+    appBarTitle = new Text(selectedMenuItem.text);
+  }
+
+  getMenuItemWidget(MenuItem item){
+    return item.func();
+  }
+
+  void onSelectItem(MenuItem item){
+    setState(() { //change the global state variables here
+      selectedMenuItem = item;
+      appBarTitle = new Text(item.text);
+    });
+
+    Navigator.of(context).pop(); //close menu
+  }
+
   @override
   Widget build(BuildContext context) {
+    menuOptionWidgets = [];
+    for(var menuItem in menuItems){
+      menuOptionWidgets.add(
+          new Container(
+            decoration: new BoxDecoration(
+              color: menuItem == selectedMenuItem ? Colors.grey[200] : Colors.white
+            ),
+            child: new ListTile(
+              onTap: () => onSelectMenuItem(menuItem),
+              title: Text(
+                menuItem.text, style: new TextStyle(
+                  fontSize: 20.0,
+                  color: menuItem.colour,
+                  fontWeight: menuItem == selectedMenuItem ? FontWeight.bold : FontWeight.w300),
+                ),
+              ),
+            ),
+          );
+
+      menuOptionWidgets.add(
+        new SizedBox(
+          child: new Center(
+            child: new Container(
+              margin: new EdgeInsetsDirectional.only(start: 20.0, end: 20.0),
+              height: 0.3,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      );
+    }
+    //------------------------------
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Main Menu"),
+        title: Text("My Courses"),
         centerTitle: true,
       ),
-      body: GridView.count(
+        //------------------------------
+        //CODE ADAPTED FROM:  https://www.linkedin.com/pulse/build-your-own-custom-side-menu-flutter-app-alon-rom
+        drawer: new Drawer(
+          child: new ListView(
+            children: <Widget>[
+              new Container(
+                  child: Center(
+                       child: Text(
+                         'Menu',
+                         textAlign: TextAlign.center,
+                         style: new TextStyle(
+                             fontSize: 20.0,
+                             fontWeight: FontWeight.bold ),
+                       ),
+                  ),
+                  margin: new EdgeInsetsDirectional.only(top: 20.0),
+                  color: Colors.white,
+                  constraints: BoxConstraints(maxHeight: 90.0, minHeight: 90.0)
+              ),
+              new SizedBox(
+                child: new Center(
+                  child: new Container(
+                    margin:
+                    new EdgeInsetsDirectional.only(start: 10.0, end: 10.0),
+                    height: 0.3,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              new Container(
+                color: Colors.white,
+                child: new Column(children: menuOptionWidgets),
+              ),
+            ],
+          ),
+        ),
+        //------------------------------
+
+
+        body: GridView.count(
         primary: false,
         padding: const EdgeInsets.all(20),
         crossAxisSpacing: 10,
@@ -93,5 +200,24 @@ class MainMenu extends StatelessWidget{
       )
 
       );
+  }
+
+  List<MenuItem> createMenuItems(){
+    final menuItems = [
+      new MenuItem(
+        text: 'My Courses'
+      ),
+      new MenuItem(
+        text: 'Settings'
+      ),
+      new MenuItem(
+        text: 'Log Out'
+      ),
+    ];
+    return menuItems;
+  }
+
+  void onSelectMenuItem(MenuItem item){
+
   }
 }
