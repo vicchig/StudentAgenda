81fa006c00798e60aab1589e44db19b1180916a6
Viktar Chyhir <vchigir12@gmail.com>
2019-10-26 12:17:18 -0400

    Added buttons for various screens that we can navigate to from the menu

--- student_agenda/lib/menuScreen.dart
+++ student_agenda/lib/menuScreen.dart
@@ -9,6 +9,31 @@ class MainMenu extends StatelessWidget{
         title: Text("Main Menu"),
         centerTitle: true,
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton( //TODO: An image would probably look better (this is just a placeholder for now)
+              onPressed: null, //TODO: Make this go to the courses page
+              child: Text('My Courses', style: TextStyle(fontSize: 20)
+              ),
+            ),
+            const SizedBox(height: 30),
+            RaisedButton(  //TODO: An image would probably look better (this is just a placeholder for now)
+              onPressed: null, //TODO: Make this go to the settings page
+              child: Text('Settings', style: TextStyle(fontSize: 20)
+              ),
+            ),
+            const SizedBox(height: 30),
+            RaisedButton(   //TODO: An image would probably look better (this is just a placeholder for now)
+              onPressed: null, //TODO: Make this go back to the log in page and log out the user
+              child: Text('Log Out', style: TextStyle(fontSize: 20)
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
