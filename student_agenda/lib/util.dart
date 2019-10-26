bc589f365e70c1011e7a79444bb9a91be304b4e5
Viktar Chyhir <vchigir12@gmail.com>
2019-10-26 13:47:00 -0400

    Updated NavigationButton wrapper with more optional params

--- student_agenda/lib/util.dart
+++ student_agenda/lib/util.dart
@@ -4,15 +4,20 @@ import 'package:flutter/material.dart';
 class NavigationButton extends StatelessWidget{
 
   String text;
+  double elevation;
+  Color colour = Colors.white;
+  double borderRad ;
+
   final GestureTapCallback onPressed;
-  NavigationButton({this.text, @required this.onPressed});
+  NavigationButton({this.text = 'Navigation Button', @required this.onPressed,
+  this.elevation = 5.0, this.colour = Colors.white, this.borderRad = 30.0});
 
   @override
   Widget build(BuildContext context){
     return Material( //Course 1
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Colors.white,
+      elevation: this.elevation,
+      borderRadius: BorderRadius.circular(this.borderRad),
+      color: this.colour,
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
