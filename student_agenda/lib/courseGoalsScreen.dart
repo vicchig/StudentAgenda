bdb6ae9e6e587e85820eead1b03bded18e86045c
Viktar Chyhir <vchigir12@gmail.com>
2019-10-26 23:52:05 -0400

    Added screen for course goals

---
+++ student_agenda/lib/courseGoalsScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+class CourseGoalsScreen extends StatefulWidget{
+  CourseGoalsScreen({Key key}) : super(key: key);
+
+  @override
+  CourseGoalsScreenState createState() => CourseGoalsScreenState();
+}
+
+class CourseGoalsScreenState extends State<CourseGoalsScreen> with TickerProviderStateMixin {
+  AnimationController logoController;
+
+  @override
+  void initState() {
+    logoController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    logoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+          title: new Text("Test Page"),
+      ),
+      body: new Text("Placeholder Text"),
+    );
+
+  }
+}
