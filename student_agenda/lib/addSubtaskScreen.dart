1cf16c40144eef1dfce631251ce9ea1757348d8e
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-11-05 15:29:08 -0500

    Fixed how the keys are set and used navigation button instead

--- student_agenda/lib/addSubtaskScreen.dart
+++ student_agenda/lib/addSubtaskScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:student_agenda/util.dart';
+import 'util.dart';
 
 class AddSubtaskScreen extends StatefulWidget {
   @override
@@ -11,70 +12,55 @@ class AddSubtaskScreen extends StatefulWidget {
 class AddSubtaskState extends State<AddSubtaskScreen> {
   List<String> subTasks = [];
   final TextEditingController subtaskController = new TextEditingController();
-  int id = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(title: new Text("Add a subtask"), centerTitle: true),
-
-        drawer: new MenuDrawer(),
-
-        body: new Column(
-          children: <Widget>[
-            new TextField(
+      appBar: new AppBar(title: new Text("Add a subtask"), centerTitle: true),
+      drawer: new MenuDrawer(),
+      body: new Column(children: <Widget>[
+        new TextField(
             controller: subtaskController,
-              onSubmitted: (text) {
-                if (text != "") {
-                  id++;
-                  subTasks.add(text);
+            onSubmitted: (text) {
+              if (text != "") {
+                subTasks.add(text);
+                subtaskController.clear();
+                setState(() {});
+              }
+            },
+            decoration: InputDecoration(hintText: "Please enter a subtask")),
+        new Expanded(
+            child: new ListView.builder(
+                itemCount: subTasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Dismissible(
+                      key: ObjectKey(subTasks[index]),
+                      onDismissed: (direction) {
+                        setState(() {
+                          subTasks.removeAt(index);
+                        });
+                        Scaffold.of(context).showSnackBar(
+                            new SnackBar(content: new Text("Subtask removed")));
+                      },
+                      child:
+                          Card(child: ListTile(title: Text(subTasks[index]))),
+                      background: new Container(color: Colors.green));
+                })),
+        new Container(
+            width: 50,
+            margin: EdgeInsets.all(24),
+            child: new NavigationButton(
+              onPressed: () {
+                if (subtaskController.text != "") {
+                  subTasks.add(subtaskController.text);
                   subtaskController.clear();
                   setState(() {});
                 }
               },
-
-              decoration: InputDecoration(
-                hintText: "Please enter a subtask"
-              )
-            ),
-            new Expanded(
-                child: new ListView.builder
-                  (
-                    itemCount: subTasks.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Dismissible  (
-                          key: new Key(id.toString()),
-                          onDismissed: (direction) {
-                            subTasks.removeAt(index);
-                            Scaffold.of(context).showSnackBar(new SnackBar (
-                                content: new Text("Subtask removed")
-                            ));
-                          },
-                          child: Card(
-                            child: ListTile(
-                              title: Text(subTasks[index])
-                            )
-                          ),
-                          background: new Container(
-                            color: Colors.green
-                          )
-                      );
-                    }
-                )
-            )
-        ]
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          if (subtaskController.text != "") {
-            id++;
-            subTasks.add(subtaskController.text);
-            subtaskController.clear();
-            setState(() {});
-          }
-        }
-      ),
+              colour: Colors.green,
+              text: "+",
+            ))
+      ]),
     );
   }
 }
