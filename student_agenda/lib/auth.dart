d283b21de914506f978de8d5289497c1556fcd5a
Josh <JoshKPrier@gmail.com>
2019-11-07 12:38:11 -0500

    Added google classroom link

--- student_agenda/lib/auth.dart
+++ student_agenda/lib/auth.dart
@@ -2,11 +2,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:googleapis/classroom/v1.dart' as classroom;
+import 'package:http/http.dart'
+    show BaseRequest, IOClient, Response, StreamedResponse;
+import 'package:http/io_client.dart';
 
 class AuthService {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = new GoogleSignIn(
+                        scopes: [
+                            'email',
+                            classroom.ClassroomApi.ClassroomCoursesScope,
+                        ],);
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final Firestore _db = Firestore.instance;
+
+  final scopes = [classroom.ClassroomApi.ClassroomCoursesScope];
 
   Observable<FirebaseUser> user; // firebase user
   Observable<Map<String, dynamic>> profile; // custom user data in Firestore
@@ -39,6 +49,16 @@ class AuthService {
     updateUserData(user);
     print("signed in " + user.displayName);
 
+    final authHeaders = _googleSignIn.currentUser.authHeaders;
+    final httpClient = new GoogleHttpClient(await authHeaders);
+
+    final data = await new classroom.ClassroomApi(httpClient).courses.list(
+      pageSize: 10,
+    );
+
+    // TODO: remove this line -- example code
+    data.courses.forEach((course) => print(course.name));
+
     loading.add(false);
     return user;
   }
@@ -59,6 +79,20 @@ class AuthService {
   void signOut() {
     _auth.signOut();
   }
+}
+
+class GoogleHttpClient extends IOClient {
+  Map<String, String> _headers;
+
+  GoogleHttpClient(this._headers) : super();
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Object url, {Map<String, String> headers}) =>
+      super.head(url, headers: headers..addAll(_headers));
 
 }
 
