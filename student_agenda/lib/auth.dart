fcc5c5563a28d6e19f6bbd5fb0a045618d902c66
Viktar Chyhir <vchigir12@gmail.com>
2019-11-08 12:11:47 -0500

    Changed existing Firestore update to be asynchronous using runTransaction

--- student_agenda/lib/auth.dart
+++ student_agenda/lib/auth.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:googleapis/appstate/v1.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:googleapis/classroom/v1.dart' as classroom;
 import 'package:http/http.dart'
     show BaseRequest, IOClient, Response, StreamedResponse;
 import 'package:http/io_client.dart';
+import 'FirestoreManager.dart';
 
 class AuthService {
   final GoogleSignIn _googleSignIn = new GoogleSignIn(
@@ -46,19 +48,20 @@ class AuthService {
         )
     )).user;
 
-    updateUserData(user);
+    doTransaction("Successfully updated user data on sign in.",
+                  "ERROR: Failed to update user data on sign in.",
+                  (){updateUserData(user);});
+
+   /* Firestore.instance.runTransaction((Transaction trans) async {
+      updateUserData(user);
+    }).then((val) => (){
+      print("Successfully updated user data on sign in.");
+    }).catchError((error) => (){
+      print("ERROR: Failed to update user data on sign in.");
+      print(error);
+    });*/
     print("signed in " + user.displayName);
-
-    final authHeaders = _googleSignIn.currentUser.authHeaders;
-    final httpClient = new GoogleHttpClient(await authHeaders);
-
-    final data = await new classroom.ClassroomApi(httpClient).courses.list(
-      pageSize: 10,
-    );/*
-
-    // TODO: remove this line -- example code
-    data.courses.forEach((course) => print(course.name));
-*/
+    
     loading.add(false);
     return user;
   }
@@ -70,13 +73,17 @@ class AuthService {
   void updateUserData(FirebaseUser user) async {
     DocumentReference ref = _db.collection('users').document(user.uid);
 
-    return ref.setData({
+    await ref.setData({
       'uid': user.uid,
       'email': user.email,
       'photoURL': user.photoUrl,
       'displayName': user.displayName,
       'lastSeen': DateTime.now()
     }, merge: true);
+
+  }
+
+  void updateUserClassroomData(FirebaseUser user) async{
 
   }
 
