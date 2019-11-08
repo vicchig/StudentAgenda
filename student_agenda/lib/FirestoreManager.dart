8365dcba088088f3d019009365b915a960896037
Viktar Chyhir <vchigir12@gmail.com>
2019-11-08 16:40:43 -0500

    Added method for pulling course information from firestore

--- student_agenda/lib/FirestoreManager.dart
+++ student_agenda/lib/FirestoreManager.dart
@@ -4,10 +4,10 @@ import 'package:googleapis/classroom/v1.dart' as classroom;
 
 import 'ClassroomApiAccess.dart';
 
-void doTransaction(String onSuccess, String onError, Function transaction){
-  try{
-    Firestore.instance.runTransaction((Transaction trans) async {
-      transaction();
+void doTransaction(String onSuccess, String onError, Function transaction) async{
+  try {
+    await Firestore.instance.runTransaction((Transaction trans) async {
+      await transaction();
     });
     print(onSuccess);
   }catch(e){
@@ -43,8 +43,28 @@ void setUserClassroomData(FirebaseUser user) async{
     mapToUpload[index.toString()] = map[index].toJson();
   });
 
-  ref.setData({
+  await ref.setData({
     "CourseObjects": mapToUpload
   }, merge: true);
 }
 
+
+Future<List<classroom.Course>> pullCourses(FirebaseUser user) async{
+  List<classroom.Course> courses = new List<classroom.Course>();
+  Map<dynamic, dynamic> courseObjectListMap;
+  try {
+    await Firestore.instance.collection("users").document(user.uid).get().then((
+        result) {
+      courseObjectListMap = result.data["CourseObjects"];
+    });
+
+
+    courseObjectListMap.entries.forEach((MapEntry<dynamic, dynamic> entry) {
+      courses.add(classroom.Course.fromJson(entry.value));
+    });
+    print("Successfully pulled courses.");
+  }catch(e){
+    print(e);
+  }
+  return courses;
+}
