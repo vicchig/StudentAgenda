a59e57274b8199c4f2da801e5896c4c5f07e76fa
Viktar Chyhir <vchigir12@gmail.com>
2019-11-11 08:58:49 -0500

    Refactor some methods and change some private fields to public for convenience.

--- student_agenda/lib/Utilities/goal.dart
+++ student_agenda/lib/Utilities/goal.dart
@@ -6,41 +6,53 @@ const String S_IN_PROGRESS = "IN_PROGRESS";
 const String S_COMPLETED   = "COMPLETED";
 const String S_COMPLETED_LATE = "COMPLETED_LATE";
 const String S_IN_PROGRESS_LATE = "IN_PROGRESS_LATE";
+const String PM = "PM";
+const String AM = "AM";
+final Map<String, String> _months = new Map<String, String>();
 
 class Goal{
   String name;
   String text;
-  classroom.Date _dueDate;
+  classroom.Date dueDate;
+  classroom.TimeOfDay dueTime;
+  String amPM;
+
   String _status;
   String _courseID;
   String _courseWorkID;
 
   Goal({String name: "BlankGoal", String text: "", courseID: "-1",
-    courseWorkID: "-1", classroom.Date dueDate}){
+    courseWorkID: "-1", String amPM: AM, classroom.Date dueDate,
+    classroom.TimeOfDay dueTime}){
 
     _status = S_IN_PROGRESS;
-    _dueDate = dueDate;
-    this.name = name;
-    this.text = text;
     _courseID = courseID;
     _courseWorkID = courseWorkID;
+
+    this.name = name;
+    this.text = text;
+    this.dueDate = dueDate;
+    this.dueTime = dueTime;
+    this.amPM = amPM;
   }
 
-
+  //TODO: Set status automatically based on what time it is (ie. past task due date, before task due date, on task due date, etc.)
   void setStatus(){
 
   }
 
-  String getDateString(){
-    return _dueDate.toString();
+  String getTextDueDate(){
+    return  "";
   }
 
-  classroom.Date getDate(){
-    return _dueDate;
+  String getTime(){
+    return "" + dueTime.hours.toString() + ":" + dueTime.minutes.toString() +
+           ":" + dueTime.seconds.toString() + amPM;
   }
 
-  void setDueDate(classroom.Date newDate){
-    _dueDate = newDate;
+  String getCalendarDueDate(){
+    return "" + dueDate.day.toString() + "/" + dueDate.month.toString() + "/" +
+        dueDate.year.toString();
   }
 
   String getCourseId(){
@@ -51,13 +63,9 @@ class Goal{
     return _courseWorkID;
   }
 
+  //TODO: Find best formatting for this
   String toString(){
-    return name + "\n" + getDateString() + "\n" + _status + "\n\n" +
-        text + "\n";
+    return name + "\n" + getCalendarDueDate() + "\n" + getTime() + "\n" +
+        _status + "\n\n" + text + "\n";
   }
-
-
-
-
-
 }
