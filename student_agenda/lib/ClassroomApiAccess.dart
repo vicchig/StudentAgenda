840fe2a9ec528251cf2d99d36f6a45b713ba6b22
Brandon Truong <b.truong@mail.utoronto.ca>
2019-11-12 15:54:18 -0500

    Added real dummy course names. Changed it so that clicking on a course leads to a page that lists dummy course works. All courses and course works are pulled from Firestore.

--- student_agenda/lib/ClassroomApiAccess.dart
+++ student_agenda/lib/ClassroomApiAccess.dart
@@ -4,25 +4,23 @@ import 'package:googleapis/classroom/v1.dart' as classroom;
 
 final Future<Map<String, String>> _authHeaders = authService.getAuthHeaders();
 
-
 /*TODO: This class currently implements dummy functions. In the future we need
 * to actually pull data from Google Classroom
 */
-class ClassroomApiAccess{
+class ClassroomApiAccess {
   static ClassroomApiAccess _instance;
 
   ClassroomApiAccess._();
 
-  static ClassroomApiAccess getInstance(){
-    if(_instance == null){
+  static ClassroomApiAccess getInstance() {
+    if (_instance == null) {
       _instance = new ClassroomApiAccess._();
     }
     return _instance;
   }
 
-
-  Future<List<classroom.Course>> getCourses() async{
-   /* // EXAMPLE CODE FOR WHEN WE ACTUALLY HAVE TO IMPLEMENT THESE
+  Future<List<classroom.Course>> getCourses() async {
+    /* // EXAMPLE CODE FOR WHEN WE ACTUALLY HAVE TO IMPLEMENT THESE
    //TODO: Should be wrapped in a try catch for the actual implementation
     GoogleHttpClient httpClient = new GoogleHttpClient(await _authHeaders);
     classroom.ListCoursesResponse courses = await new classroom.ClassroomApi
@@ -32,8 +30,19 @@ class ClassroomApiAccess{
     return courses.courses; will be null for now since there are no courses,
      BUT return courses should never be null, if it is, there is an error*/
 
+    const List<String> courseNames = <String>[
+      'Mathematics',
+      'Language',
+      'French as a Second Language',
+      'Health and Physical Education',
+      'Science and Technology',
+      'The Arts',
+      'Social Studies',
+      'History and Geography'
+    ];
+
     List<classroom.Course> courses = new List<classroom.Course>();
-    for(int i = 0; i < 8; i++){
+    for (int i = 0; i < 8; i++) {
       courses.add(new classroom.Course());
       courses[i].id = i.toString();
       courses[i].calendarId = "calendarID" + i.toString();
@@ -41,7 +50,7 @@ class ClassroomApiAccess{
       courses[i].courseGroupEmail = "classmail@gmail.com" + i.toString();
       courses[i].enrollmentCode = "enrollment code" + i.toString();
       courses[i].description = "Course description" + i.toString();
-      courses[i].name = "Course Name " + i.toString();
+      courses[i].name = courseNames[i];
     }
 
     return courses;
@@ -50,25 +59,37 @@ class ClassroomApiAccess{
   Future<List<classroom.CourseWork>> getCourseWork() async {
     List<classroom.CourseWork> courseWorks = new List<classroom.CourseWork>();
     int count = 0;
-    for(int i = 0; i < 8; i++){ //for every dummy course
-      for(int j = 0; j < 5; j++){ //create 5 course works
+
+    for (int i = 0; i < 8; i++) {
+      //for every dummy course
+      for (int j = 0; j < i + 3; j++) {
+        //create 5 course works
         courseWorks.add(new classroom.CourseWork());
         courseWorks[count].courseId = i.toString();
-        courseWorks[count].description = "Assignment Description";
+        courseWorks[count].description = "Hand in Workbook Page ${j + 10}";
 
         classroom.Date date = new classroom.Date();
-        date.month = 1;
-        date.day = 15;
-        date.year = 1990;
-        courseWorks[count].dueDate = date;
+        date.month = DateTime
+            .now()
+            .add(new Duration(days: j + 7))
+            .month;
+        date.day = DateTime
+            .now()
+            .add(new Duration(days: j + 7))
+            .day;
+        date.year = DateTime
+            .now()
+            .add(new Duration(days: j + 7))
+            .year;
+        date = courseWorks[count].dueDate = date;
 
         courseWorks[count].id = i.toString() + j.toString();
         courseWorks[count].maxPoints = 10.0;
 
         classroom.TimeOfDay time = new classroom.TimeOfDay();
-        time.hours = 10;
-        time.minutes = 59;
-        time.seconds = 59;
+        time.hours = 8;
+        time.minutes = 5;
+        time.seconds = 0;
         courseWorks[count].dueTime = time;
 
         courseWorks[count].scheduledTime = "Scheduled Time";
@@ -82,10 +103,12 @@ class ClassroomApiAccess{
 
   Future<List<classroom.Announcement>> getAnnouncements() async {
     List<classroom.Announcement> announcements =
-                                            new List<classroom.Announcement>();
+    new List<classroom.Announcement>();
     int count = 0;
-    for(int i = 0; i < 8; i++){ //for every dummy course
-      for(int j = 0; j < 2; j++){ //create 5 announcements per course
+    for (int i = 0; i < 8; i++) {
+      //for every dummy course
+      for (int j = 0; j < 2; j++) {
+        //create 5 announcements per course
         announcements.add(new classroom.Announcement());
         announcements[count].courseId = i.toString();
         announcements[count].text = "Announcement Text";
@@ -97,9 +120,11 @@ class ClassroomApiAccess{
 
   Future<List<classroom.Student>> getStudents() async {
     List<classroom.Student> students = new List<classroom.Student>();
-    int count =  0;
-    for(int i = 0; i < 8; i++){ //for every dummy course
-      for(int j = 0; j < 5; j++){ //create 5 students per course
+    int count = 0;
+    for (int i = 0; i < 8; i++) {
+      //for every dummy course
+      for (int j = 0; j < 5; j++) {
+        //create 5 students per course
         students.add(new classroom.Student());
         students[count].courseId = i.toString();
         students[count].userId = "UID" + (count).toString();
@@ -116,7 +141,8 @@ class ClassroomApiAccess{
   Future<List<classroom.Teacher>> getTeachers() async {
     List<classroom.Teacher> teachers = new List<classroom.Teacher>();
 
-    for(int i = 0; i < 8; i++){ //for every dummy course
+    for (int i = 0; i < 8; i++) {
+      //for every dummy course
       teachers.add(new classroom.Teacher());
       teachers[i].courseId = i.toString();
       teachers[i].profile = new classroom.UserProfile();
@@ -130,8 +156,10 @@ class ClassroomApiAccess{
   Future<List<classroom.Topic>> getTopics() async {
     List<classroom.Topic> topics = new List<classroom.Topic>();
     int count = 0;
-    for(int i = 0; i < 8; i++){ //for every dummy course
-      for(int j = 0; j < 5; j++){ //create 5 topics per course
+    for (int i = 0; i < 8; i++) {
+      //for every dummy course
+      for (int j = 0; j < 5; j++) {
+        //create 5 topics per course
         topics.add(new classroom.Topic());
         topics[count].name = "Topic Name " + j.toString();
         topics[count].courseId = i.toString();
