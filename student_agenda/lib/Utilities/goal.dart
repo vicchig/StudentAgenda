26edd7856f15402f320f0a38ded95c82216b894e
Viktar Chyhir <vchigir12@gmail.com>
2019-11-12 11:30:39 -0500

    Finish manual testing of methods

--- student_agenda/lib/Utilities/goal.dart
+++ student_agenda/lib/Utilities/goal.dart
@@ -6,21 +6,19 @@ const String S_IN_PROGRESS = "IN_PROGRESS";
 const String S_COMPLETED   = "COMPLETED";
 const String S_COMPLETED_LATE = "COMPLETED_LATE";
 const String S_IN_PROGRESS_LATE = "IN_PROGRESS_LATE";
-const String PM = "PM";
-const String AM = "AM";
 
 class Goal{
   String name;
   String text;
   DateTime dueDate;
-  String amPM;
 
   String _status;
   String _courseID;
   String _courseWorkID;
 
+  //the dueDate string should be in the following format "yyyy-mm-ddThh:mm:ss"
   Goal({String name: "BlankGoal", String text: "", courseID: "-1",
-    courseWorkID: "-1", String amPM: AM, String dueDate}){
+    courseWorkID: "-1",  String dueDate: ""}){
 
     _status = S_IN_PROGRESS;
     _courseID = courseID;
@@ -28,11 +26,10 @@ class Goal{
 
     this.name = name;
     this.text = text;
-    this.dueDate = DateTime.parse(dueDate); //TODO: make sure to tell them the format for this
+    this.dueDate = DateTime.parse(dueDate);
   }
 
-  //TODO: Set status automatically based on what time it is (ie. past task due date, before task due date, on task due date, etc.)
-  //The complete status shouldbe set through the method
+  //The complete status should be set through the method
   void setStatus(){
     DateTime currDateTime = DateTime.now();
     if(currDateTime.isAfter(dueDate) && _status == S_IN_PROGRESS){
@@ -50,15 +47,18 @@ class Goal{
     _status = S_COMPLETED;
   }
 
-  //TODO: test this
+  //returns the time this task is due (DOES NOT DISTINGUISH BETWEEN AM/PM,
+  // SO USE THE 24 HOUR CLOCK)
   String getDueTime(){
     String result = dueDate.toIso8601String();
     result = result.substring(result.indexOf("T"));
     List<String> timeParts = result.split(":");
-    return ""+timeParts[0] + ":" + timeParts[1] + ":" + timeParts[2].substring(0, 2);
+    return ""+timeParts[0].substring(1,) + ":" + timeParts[1] + ":" +
+        timeParts[2].substring(0, 2);
   }
 
-  //TODO: Make sure the formatting on this is good
+  //Returns the calendar due date for this task in the format "dd/mm/yyyy"
+  //Can extract the month by calling util.getMonthFromDateStr(dateStr);
   String getCalendarDueDate(){
     return "" + dueDate.day.toString() + "/" + dueDate.month.toString() + "/" +
         dueDate.year.toString();
@@ -72,9 +72,8 @@ class Goal{
     return _courseWorkID;
   }
 
-  //TODO: Find best formatting for this
   String toString(){
     return name + "\n" + getCalendarDueDate() + "\n" + getDueTime() + "\n" +
-        _status + "\n\n" + text + "\n";
+        _status + "\n\n" + text + "\n\n";
   }
 }
