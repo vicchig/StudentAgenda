1dc5794b181d3adf6423570a24ee13e05ab481e5
Robert <robert.fang@mail.utoronto.ca>
2019-11-21 22:03:43 -0500

    Add input text field for 'Other' goals

--- student_agenda/lib/Screens/addGoalsScreen.dart
+++ student_agenda/lib/Screens/addGoalsScreen.dart
@@ -36,14 +36,11 @@ class AddGoalsScreenState extends State<AddGoalsScreen> {
     processFuture();
   }
 
-
-
   static var subtasks = ['Write the intro paragraph', 'Write the first body', 'Write the body paragraphs','Write the conclusion', 'Other'];
   var selectedSubtask = null; // NOTE: Depending on implementation, may need to check for empty
   var selectedCourse = null;
 
-
-  var otherSelected = false;
+  String otherSubtask = null;
 
 
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -72,25 +69,70 @@ class AddGoalsScreenState extends State<AddGoalsScreen> {
       //draw the sidebar menu options
       drawer: new MenuDrawer(),
 
-      body: Column(
+      body: ListView(
       children: [
       
       SizedBox(height: 140),
+
       courseDropbox(context),
+
       SizedBox(height: 20),
 
       subtaskDropbox(context),
-      
+
+      (selectedSubtask == 'Other') ? SizedBox(height: 20) : SizedBox(height: 0),
+
+      (selectedSubtask == 'Other') ? otherTextfield(context) : SizedBox(height: 0),
+
       SizedBox(height: 20),
 
       datepicker(context),
       
       SizedBox(height: 80),
-      
-      addButton(context),
+      Align(alignment: Alignment.center, child:addButton(context))
       ]
     ),
     );
+  }
+
+
+
+
+  Widget otherTextfield(BuildContext context) {
+    Widget textField =   Container(
+      child: TextField(
+        style: TextStyle(fontSize: 18,
+            color: Colors.green),
+        decoration: InputDecoration(
+          hintStyle: TextStyle(fontSize: 18, fontStyle: FontStyle.italic,
+              color: (selectedCourse == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.greenAccent),
+          hintText: 'What goal did you have in mind?',
+          border: InputBorder.none,
+        ),
+        onChanged: (text) {_setOtherSubtask(text);},
+      ),
+    );
+
+    Widget alignedBox = Align(
+      alignment: Alignment(0.0, -0.7),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.green,
+              width: 3,
+            ),
+            borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(10.0),
+                topRight: const Radius.circular(10.0),
+                bottomLeft: const Radius.circular(10.0),
+                bottomRight: const Radius.circular(10.0))),
+        height: 50,
+        width: 330,
+        padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+        child: textField,
+      ),
+    );
+    return alignedBox;
   }
 
   Widget courseDropbox(BuildContext context) {
@@ -102,7 +144,7 @@ class AddGoalsScreenState extends State<AddGoalsScreen> {
         items: _courses.map((classroom.Course dropDownItem) {
           return DropdownMenuItem<classroom.Course>(
             value: dropDownItem,
-            child: Text((dropDownItem.ownerId != null) ? dropDownItem.ownerId : "Nobody" + "'s " + dropDownItem.name.toString(),
+            child: Text((dropDownItem.ownerId != null) ? dropDownItem.ownerId : "Someone" + "'s " + dropDownItem.name.toString(),
               overflow: TextOverflow.ellipsis,),
           );
         }).toList(),
@@ -193,10 +235,17 @@ class AddGoalsScreenState extends State<AddGoalsScreen> {
           ),
         ),
       );
-     
-     
+
+
     return alignedBox;
   }
+
+  void _setOtherSubtask(String otherSubtask) {
+    setState(() {
+      this.otherSubtask = otherSubtask;
+    });
+  }
+
   void _onDropDownItemSelectedCourse(classroom.Course newValueSelected) {
     setState(() {
       this.selectedCourse = newValueSelected;
@@ -210,63 +259,74 @@ class AddGoalsScreenState extends State<AddGoalsScreen> {
   }
 
   Widget datepicker(BuildContext context) {
+
+    Widget flatButton = FlatButton(
+      onPressed: () => _selectDate(context),
+      textColor: (selectedCourse == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green,
+
+      child: Text(
+          "${months[selectedDate.month - 1]} ${selectedDate.day}, ${selectedDate.year}",
+
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)
+      ),
+    );
+
     Widget retDate =
-      IgnorePointer(
-        ignoring: selectedCourse == null,
-        ignoringSemantics: selectedCourse == null,
-        child:FlatButton(
-            onPressed: () => _selectDate(context),
-            textColor: (selectedCourse == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green,
-            color: Colors.white,
-            padding: const EdgeInsets.all(0.0),
-            shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(10.0),
-            side: BorderSide(color: (selectedCourse == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green, width: 3)),
-
-          child: Container(
-            height: 50,
-            width: 330,
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-            child: Text(
-              "${months[selectedDate.month - 1]} ${selectedDate.day}, ${selectedDate.year}",
-
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)
+        IgnorePointer(
+          ignoring: selectedCourse == null,
+          ignoringSemantics: selectedCourse == null,
+          child: Align(
+            alignment: Alignment(0.0, -0.7),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(226,240,217, 1.0),
+                  border: Border.all(
+                    color: (selectedCourse == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green,
+                    width: 3,
+                  ),
+                  borderRadius: new BorderRadius.only(
+                      topLeft: const Radius.circular(10.0),
+                      topRight: const Radius.circular(10.0),
+                      bottomLeft: const Radius.circular(10.0),
+                      bottomRight: const Radius.circular(10.0))),
+              height: 50,
+              width: 330,
+              padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+              child: flatButton,
             ),
           ),
+        );
 
-          ),
-      );
-      
+
     return retDate;
   }
-  
+
   Widget addButton(BuildContext context) {
-      Widget retButton =
-        IgnorePointer(
-          ignoring:selectedCourse == null || selectedSubtask == null,
-          ignoringSemantics: selectedCourse == null || selectedSubtask == null,
-          child:FlatButton(
-            onPressed: () => finalizeSubtask(),
-                textColor: Colors.white,
-                color: (selectedCourse == null || selectedSubtask == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green,
-                padding: const EdgeInsets.all(0.0),
-                shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(10.0),
-                side: BorderSide(color:(selectedCourse == null || selectedSubtask == null) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green, width: 3)),
-            child: Container(
-                height: 70,
-                width: 100,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10.0),
-                child: Text('Add',
+    Widget retButton =
+    IgnorePointer(
+        ignoring:selectedCourse == null || selectedSubtask == null || (selectedSubtask == 'Other' && otherSubtask == null),
+        ignoringSemantics: selectedCourse == null || selectedSubtask == null || (selectedSubtask == 'Other' && otherSubtask == null),
+        child:FlatButton(
+          onPressed: () => finalizeSubtask(),
+          textColor: Colors.white,
+          color: (selectedCourse == null || selectedSubtask == null || (selectedSubtask == 'Other' && otherSubtask == null)) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green,
+          padding: const EdgeInsets.all(0.0),
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(10.0),
+              side: BorderSide(color:(selectedCourse == null || selectedSubtask == null || (selectedSubtask == 'Other' && otherSubtask == null)) ? Color.fromRGBO(200,200,200, 1.0) : Colors.green, width: 3)),
+          child: Container(
+            height: 70,
+            width: 100,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10.0),
+            child: Text('Add',
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700)),
-            ),
-          )
-      );
+          ),
+        )
+    );
     return retButton;
   }
-  
+
   void finalizeSubtask() async {
 
       List<classroom.Course> courses = await pullCourses(firebaseUser);
@@ -275,16 +335,17 @@ class AddGoalsScreenState extends State<AddGoalsScreen> {
       }
 
 
-      Goal subtask = new Goal(name: "Placeholder for goal name",
-                              text: selectedSubtask,
+      Goal subtask = new Goal(name: (selectedSubtask == 'Other') ? otherSubtask:selectedSubtask,
         dueDate: selectedDate.toString(),
         courseID: selectedCourse.id,
       );
 
+
+
       subtask.setStatus();
-      List<Goal> subtasks = await pullGoals(firebaseUser, "CourseWorkGoalObjects");
+      List<Goal> subtasks = await pullGoals(firebaseUser, "CourseGoalObjects");
       subtasks.add(subtask);
-      setUserCourseGoals(firebaseUser, subtasks, "CourseWorkGoalObjects");
+      setUserCourseGoals(firebaseUser, subtasks, "CourseGoalObjects");
       
       
   }
