d3496efc7f3ca1cf6a344a24c5b19361caa95de4
Brandon Truong <b.truong@mail.utoronto.ca>
2019-11-26 06:06:31 -0500

    Recreated the Goals Lists screen to pull data from Firestore. Added the ability to complete the goal. Main dashboard screen is also updated to reflect these changes. Connected the Goals button in the bottom navigation bar.

--- student_agenda/lib/Screens/mainScreen.dart
+++ student_agenda/lib/Screens/mainScreen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rounded_progress_bar/flutter_rounded_progress_bar.dart';
 import 'package:flutter_rounded_progress_bar/rounded_progress_bar_style.dart';
-import 'package:student_agenda/Screens/listedGoalsScreen.dart';
+import 'package:intl/intl.dart';
 import 'package:student_agenda/Screens/performanceScreen.dart';
-import 'package:student_agenda/tasks.dart';
 import 'package:student_agenda/Utilities/util.dart';
+import 'package:student_agenda/Utilities/goal.dart';
+import 'package:student_agenda/FirestoreManager.dart';
+import 'package:student_agenda/Utilities/auth.dart';
+import 'package:googleapis/classroom/v1.dart' as classroom;
+import 'package:student_agenda/Screens/listedGoalsScreen.dart';
 
 class MainDashboardScreen extends StatefulWidget {
   MainDashboardScreen({Key key, this.title}) : super(key: key);
@@ -17,6 +21,40 @@ class MainDashboardScreen extends StatefulWidget {
 
 class _MainDashboardScreenState extends State<MainDashboardScreen> {
   double performancePercent = 70;
+
+  List<Goal> _goals = new List<Goal>();
+  List<classroom.Course> _courses = new List<classroom.Course>();
+  List<classroom.CourseWork> _courseWorks = new List<classroom.CourseWork>();
+
+  Future<void> processFuture() async {
+    List<Goal> tempGoals =
+    await pullGoals(firebaseUser, "CourseWorkGoalObjects");
+    tempGoals.addAll(await pullGoals(firebaseUser, "CourseGoalObjects"));
+    tempGoals.sort((a, b) =>
+        DateTime(a.dueDate.year, a.dueDate.month,
+            a.dueDate.day, a.dueDate.hour, a.dueDate.minute, a.dueDate.second)
+            .compareTo(DateTime(b.dueDate.year, b.dueDate.month, b.dueDate.day,
+            b.dueDate.hour, b.dueDate.minute, b.dueDate.second)));
+
+    List<classroom.Course> tempCourses = await pullCourses(firebaseUser);
+
+    List<classroom.CourseWork> tempCourseWorks =
+    await pullCourseWorkData(firebaseUser);
+
+    setState(() {
+      _goals = tempGoals;
+      _courses = tempCourses;
+      _courseWorks = tempCourseWorks;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    processFuture().then((arg) {}, onError: (e) {
+      print(e);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,9 +95,19 @@ class _MainDashboardScreenState extends State<MainDashboardScreen> {
           ),
           SizedBox(height: 20),
           Heading(text: 'Upcoming Tasks'),
-          TaskList(length: Task.dates.length),
+          Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: (_goals.length > 3) ? 3 + 1 : _goals.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                return buildList(index);
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  SizedBox(height: 10),
+            ),
+          ),
           Center(
-            child: (Task.dates.length > 1)
+            child: (_goals.length > 1)
                 ? FlatButton(
               color: Colors.green,
               shape: RoundedRectangleBorder(
@@ -86,6 +134,121 @@ class _MainDashboardScreenState extends State<MainDashboardScreen> {
       ),
     );
   }
+
+  Color getBoxColor(index) {
+    if (index != 0) {
+      int realIndex = index - 1;
+      if (_goals[realIndex].getStatus() != S_COMPLETED &&
+          _goals[realIndex].getStatus() != S_COMPLETED_LATE) {
+        return Colors.green[100];
+      } else {
+        return Colors.grey;
+      }
+    }
+    return Colors.red;
+  }
+
+  Container buildList(index) {
+    if (index == 0) {
+      return Container(width: 0, height: 0);
+    } else {
+      int realIndex = index - 1;
+
+      return Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: getBoxColor(index),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Course: ${_courses
+                  .firstWhere((course) =>
+              course.id == _goals[realIndex].getCourseId(),
+                  orElse: () => classroom.Course())
+                  .name}',
+              style: TextStyle(fontSize: 18),
+            ),
+            (_goals[realIndex].getCourseWorkId() != "-1")
+                ? Text(
+              'Course Work: ${_courseWorks
+                  .firstWhere((courseWork) =>
+              courseWork.courseId == _goals[realIndex].getCourseId() &&
+                  courseWork.id == _goals[realIndex].getCourseWorkId(),
+                  orElse: () => classroom.CourseWork())
+                  .description}'
+                  .replaceAll(new RegExp(r"null$"), "N/A"),
+              style: TextStyle(fontSize: 18),
+            )
+                : Container(width: 0, height: 0),
+            Text(
+              'Tasks: ${_goals[realIndex].name}',
+              style: TextStyle(fontSize: 18),
+            ),
+            (_goals[realIndex].text) != ""
+                ? Text(
+              'Description: ${_goals[realIndex].text}',
+              style: TextStyle(fontSize: 18),
+            )
+                : Container(width: 0, height: 0),
+            Text(
+              'Due: ${DateFormat("EEEE, MMM. d, yyyy").format(DateTime(
+                  _goals[realIndex].dueDate.year,
+                  _goals[realIndex].dueDate.month,
+                  _goals[realIndex].dueDate.day, _goals[realIndex].dueDate.hour,
+                  _goals[realIndex].dueDate.minute,
+                  _goals[realIndex].dueDate.second))}',
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              'Status: ${_goals[realIndex].getStatus().replaceAll("_", " ")}',
+              style: TextStyle(fontSize: 18),
+            ),
+            completeGoalButton(index)
+          ],
+        ),
+      );
+    }
+  }
+
+  RaisedButton completeGoalButton(index) {
+    if (index != 0) {
+      int realIndex = index - 1;
+      if (_goals[realIndex].getStatus() != S_COMPLETED &&
+          _goals[realIndex].getStatus() != S_COMPLETED_LATE) {
+        return RaisedButton(
+          onPressed: () {
+            setState(() {
+              _goals[realIndex].completeGoal();
+              List<Goal> goal = List<Goal>();
+              goal.add(_goals[realIndex]);
+              setUserCourseGoals(
+                  firebaseUser,
+                  goal,
+                  (_goals[realIndex].getCourseWorkId() == "-1")
+                      ? "CourseGoalObjects"
+                      : "CourseWorkGoalObjects",
+                  toMerge: true);
+            });
+          },
+          child:
+          const Text('Complete This Goal', style: TextStyle(fontSize: 20)),
+        );
+      } else {
+        return RaisedButton(
+          onPressed: null,
+          child:
+          const Text('Complete This Goal', style: TextStyle(fontSize: 20)),
+        );
+      }
+    }
+    return RaisedButton(
+      onPressed: null,
+      child: const Text('Complete This Goal', style: TextStyle(fontSize: 20)),
+    );
+  }
 }
 
 class Heading extends StatelessWidget {
@@ -108,47 +271,6 @@ class Heading extends StatelessWidget {
           fontWeight: FontWeight.bold,
           fontSize: 17,
         ),
-      ),
-    );
-  }
-}
-
-class TaskList extends StatelessWidget {
-  final int length;
-
-  const TaskList({
-    Key key,
-    @required this.length,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    if (length == 0) {
-      return Expanded(
-        child: Container(
-          alignment: Alignment.center,
-          child: Text(
-            'No Upcoming Tasks',
-            textAlign: TextAlign.start,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
-              color: Colors.red,
-            ),
-          ),
-        ),
-      );
-    }
-
-    return Expanded(
-      child: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (context, index) {
-          final date = Task.dates[index];
-
-          return Task.subtaskDropbox(context, date);
-        },
       ),
     );
   }
