7de52c3cc2e76547bc0bed5c8d22d1db77d756df
Brandon Truong <b.truong@mail.utoronto.ca>
2019-11-30 07:04:59 -0500

    Fixed bug in the Goal class caused by a null completion time. Updated Task List screen and the main screen to show the date assigned and date completed (if applicable).

--- student_agenda/lib/Screens/mainScreen.dart
+++ student_agenda/lib/Screens/mainScreen.dart
@@ -207,6 +207,15 @@ class _MainDashboardScreenState extends State<MainDashboardScreen> {
               )
                   : Container(width: 0, height: 0),
               Text(
+                'Date Assigned: ${DateFormat("yyyy/MM/dd hh:mm aaa").format(
+                    _goals[index].getDateAssigned())}',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Status: ${_goals[index].getStatus().replaceAll("_", " ")}',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
                 'Due: ${DateFormat("EEEE, MMM. d, yyyy").format(DateTime(
                     _goals[index].dueDate.year, _goals[index].dueDate.month,
                     _goals[index].dueDate.day, _goals[index].dueDate.hour,
@@ -214,10 +223,14 @@ class _MainDashboardScreenState extends State<MainDashboardScreen> {
                     _goals[index].dueDate.second))}',
                 style: TextStyle(fontSize: 18),
               ),
-              Text(
-                'Status: ${_goals[index].getStatus().replaceAll("_", " ")}',
+              (_goals[index].getStatus() == S_COMPLETED ||
+                  _goals[index].getStatus() == S_COMPLETED_LATE)
+                  ? Text(
+                'Date Completed: ${DateFormat("yyyy/MM/dd hh:mm aaa").format(
+                    _goals[index].getDateCompleted())}',
                 style: TextStyle(fontSize: 18),
-              ),
+              )
+                  : Container(width: 0, height: 0),
               completeGoalButton(index)
             ],
           ),
