279b66d2112e06c3443727211bed225e3c9ab416
Michael-Chunman-Lee <michaelchunman.lee@mail.utoronto.ca>
2019-12-01 14:33:53 -0500

    Reformatted the code

--- student_agenda/lib/Screens/classView.dart
+++ student_agenda/lib/Screens/classView.dart
@@ -14,9 +14,7 @@ class ClassViewScreen extends StatefulWidget {
   final String courseID;
 
   @override
-  ClassViewState createState() =>
-      ClassViewState(courseID: courseID);
-
+  ClassViewState createState() => ClassViewState(courseID: courseID);
 }
 
 class ClassViewState extends State<ClassViewScreen> {
@@ -27,10 +25,14 @@ class ClassViewState extends State<ClassViewScreen> {
   List<classroom.Teacher> _teachers = new List<classroom.Teacher>();
 
   Future<void> retrieveStudents() async {
-    List<classroom.Student> allSubscribedStudents = await pullClassmates(firebaseUser);
-    List<classroom.Student> tempStudents = getClassRoster(courseID, allSubscribedStudents);
-    List<classroom.Teacher> allSubscribedTeachers = await pullTeachers(firebaseUser);
-    List<classroom.Teacher> tempTeachers = await getCourseTeachers(courseID, allSubscribedTeachers);
+    List<classroom.Student> allSubscribedStudents =
+        await pullClassmates(firebaseUser);
+    List<classroom.Student> tempStudents =
+        getClassRoster(courseID, allSubscribedStudents);
+    List<classroom.Teacher> allSubscribedTeachers =
+        await pullTeachers(firebaseUser);
+    List<classroom.Teacher> tempTeachers =
+        await getCourseTeachers(courseID, allSubscribedTeachers);
 
     setState(() {
       _students = tempStudents;
@@ -48,63 +50,54 @@ class ClassViewState extends State<ClassViewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(title: new Text("Class Roster"), centerTitle: true),
-
       drawer: new MenuDrawer(),
-
-      body: new Column(
-          children: <Widget>[
-            Text("\n\nTeachers\n", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            new Expanded(
-                child: new ListView.builder
-                  (
-                    itemCount: _teachers.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return new Card(
-                          child: ListTile(
-                            leading: Icon(Icons.mood),
-                            title: Text(_teachers[index].profile.name.fullName),
-                          )
-                      );
-                    }
-                )
-            ),
-            Text("\nStudents\n", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            new Expanded(
-                child: new ListView.builder
-                  (
-                    itemCount: _students.length,
-                    itemBuilder: (BuildContext context, int index) {
-                          return new Card(
-                              child: ListTile(
-                                leading: Icon(Icons.mood),
-                                  title: Text(_students[index].profile.name.fullName),
-                              )
-                          );
-                    }
-                )
-            ),
-            FlatButton(
-              color: Colors.green,
-              shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(10.0),
-                side: BorderSide(color: Colors.green, width: 3),
-              ),
-              textColor: Colors.white,
-              padding: EdgeInsets.all(8.0),
-              splashColor: Colors.blueAccent,
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => teacherAssignmentScreen(courseID: courseID)));
-              },
-              child: Text(
-                "View Course Assignments",
-                style: TextStyle(fontSize: 20.0),
-              ),
-            )
-          ]
-      ),
+      body: new Column(children: <Widget>[
+        Text("\n\nTeachers\n",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        new Expanded(
+            child: new ListView.builder(
+                itemCount: _teachers.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new Card(
+                      child: ListTile(
+                    leading: Icon(Icons.mood),
+                    title: Text(_teachers[index].profile.name.fullName),
+                  ));
+                })),
+        Text("\nStudents\n",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        new Expanded(
+            child: new ListView.builder(
+                itemCount: _students.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new Card(
+                      child: ListTile(
+                    leading: Icon(Icons.mood),
+                    title: Text(_students[index].profile.name.fullName),
+                  ));
+                })),
+        FlatButton(
+          color: Colors.green,
+          shape: RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(10.0),
+            side: BorderSide(color: Colors.green, width: 3),
+          ),
+          textColor: Colors.white,
+          padding: EdgeInsets.all(8.0),
+          splashColor: Colors.blueAccent,
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        teacherAssignmentScreen(courseID: courseID)));
+          },
+          child: Text(
+            "View Course Assignments",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        )
+      ]),
     );
   }
 }
--- student_agenda/lib/Screens/teacherViewAssignments.dart
+++ student_agenda/lib/Screens/teacherViewAssignments.dart
@@ -16,7 +16,6 @@ class teacherAssignmentScreen extends StatefulWidget {
   @override
   teacherAssignmentState createState() =>
       teacherAssignmentState(courseID: courseID);
-
 }
 
 class teacherAssignmentState extends State<teacherAssignmentScreen> {
@@ -26,8 +25,10 @@ class teacherAssignmentState extends State<teacherAssignmentScreen> {
   List<classroom.CourseWork> _courseWork = new List<classroom.CourseWork>();
 
   Future<void> retrieveStudents() async {
-    List<classroom.CourseWork> allSubscribedCourseWork = await pullCourseWorkData(firebaseUser);
-    List<classroom.CourseWork> tempCourseWork = await getCourseWorksForCourse(courseID, allSubscribedCourseWork);
+    List<classroom.CourseWork> allSubscribedCourseWork =
+        await pullCourseWorkData(firebaseUser);
+    List<classroom.CourseWork> tempCourseWork =
+        await getCourseWorksForCourse(courseID, allSubscribedCourseWork);
 
     setState(() {
       _courseWork = tempCourseWork;
@@ -43,58 +44,47 @@ class teacherAssignmentState extends State<teacherAssignmentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(title: new Text("Class Assignments"), centerTitle: true),
-
+      appBar:
+          new AppBar(title: new Text("Class Assignments"), centerTitle: true),
       drawer: new MenuDrawer(),
-
-      body: new Column(
-          children: <Widget>[
-            Text("\n\Assignments\n", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            new Expanded(
-                child: new ListView.builder
-                  (
-                    itemCount: _courseWork.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return new Card(
-                          child: ListTile(
-                            leading: Icon(Icons.book),
-                            title: Text(_courseWork[index].description),
-                            subtitle:     Text(
-                              'Due: ${DateTime(_courseWork[index].dueDate.year,
-                                  _courseWork[index].dueDate.month,
-                                  _courseWork[index].dueDate.day, _courseWork[index]
-                                      .dueTime?.hours ?? 0, _courseWork[index].dueTime
-                                      ?.minutes ?? 0).toString().replaceAll(
-                                  new RegExp("\\..+"), "")}',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          )
-                      );
-                    }
-                )
-            ),
-            FlatButton(
-              color: Colors.green,
-              shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(10.0),
-                side: BorderSide(color: Colors.green, width: 3),
-              ),
-              textColor: Colors.white,
-              padding: EdgeInsets.all(8.0),
-              splashColor: Colors.blueAccent,
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ClassViewScreen(courseID: courseID)));
-              },
-              child: Text(
-                "View Course Roster",
-                style: TextStyle(fontSize: 20.0),
-              ),
-            )
-          ]
-      ),
+      body: new Column(children: <Widget>[
+        Text("\n\Assignments\n",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        new Expanded(
+            child: new ListView.builder(
+                itemCount: _courseWork.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new Card(
+                      child: ListTile(
+                    leading: Icon(Icons.book),
+                    title: Text(_courseWork[index].description),
+                    subtitle: Text(
+                      'Due: ${DateTime(_courseWork[index].dueDate.year, _courseWork[index].dueDate.month, _courseWork[index].dueDate.day, _courseWork[index].dueTime?.hours ?? 0, _courseWork[index].dueTime?.minutes ?? 0).toString().replaceAll(new RegExp("\\..+"), "")}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ));
+                })),
+        FlatButton(
+          color: Colors.green,
+          shape: RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(10.0),
+            side: BorderSide(color: Colors.green, width: 3),
+          ),
+          textColor: Colors.white,
+          padding: EdgeInsets.all(8.0),
+          splashColor: Colors.blueAccent,
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ClassViewScreen(courseID: courseID)));
+          },
+          child: Text(
+            "View Course Roster",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        )
+      ]),
     );
   }
 }
